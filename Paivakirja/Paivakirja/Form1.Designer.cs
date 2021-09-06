7da31f4a8b03f327f5f4f66c6a4a163408b76907
micostrom <63344421+micostrom@users.noreply.github.com>
2021-09-06 13:54:40 +0300

    Add files via upload

---
+++ Paivakirja/Paivakirja/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Paivakirja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SyottoTB = new System.Windows.Forms.TextBox();
+            this.TallennaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SyottoTB
+            // 
+            this.SyottoTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SyottoTB.Location = new System.Drawing.Point(7, 7);
+            this.SyottoTB.Multiline = true;
+            this.SyottoTB.Name = "SyottoTB";
+            this.SyottoTB.Size = new System.Drawing.Size(786, 345);
+            this.SyottoTB.TabIndex = 0;
+            // 
+            // TallennaBT
+            // 
+            this.TallennaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TallennaBT.Location = new System.Drawing.Point(7, 358);
+            this.TallennaBT.Name = "TallennaBT";
+            this.TallennaBT.Size = new System.Drawing.Size(785, 87);
+            this.TallennaBT.TabIndex = 1;
+            this.TallennaBT.Text = "Tallenna päiväkirjaan";
+            this.TallennaBT.UseVisualStyleBackColor = true;
+            this.TallennaBT.Click += new System.EventHandler(this.TallennaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TallennaBT);
+            this.Controls.Add(this.SyottoTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SyottoTB;
+        private System.Windows.Forms.Button TallennaBT;
+    }
+}
+
---
+++ Paivakirja/Paivakirja/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Paivakirja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TallennaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = File.ReadAllText(@"C:\\Users\\mico.strom\\source\\repos\\Paivakirja\\Paivakirja.txt");
+            teksti += SyottoTB.Text;
+            teksti += " " + DateTime.Now.ToString("dd,MM,yyyy HH:mm") + "\n";
+            TextWriter text = new StreamWriter("C:\\Users\\mico.strom\\source\\repos\\Paivakirja\\Paivakirja.txt");
+            text.Write(teksti);
+            text.Close();
+            Application.Exit();
+        }
+    }
+}
