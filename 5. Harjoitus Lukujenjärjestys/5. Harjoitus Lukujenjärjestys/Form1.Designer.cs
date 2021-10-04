e8acf2b3b775f1b2deaf196cac40048e8a83017a
micostrom <63344421+micostrom@users.noreply.github.com>
2021-10-04 04:52:42 +0300

    Add files via upload

---
+++ 5. Harjoitus Lukujenjärjestys/5. Harjoitus Lukujenjärjestys/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace _5._Harjoitus_Lukujenjärjestys
+{
+    partial class Lukujenjarjestys
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UusilukuTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku (-999 lopetus):";
+            // 
+            // UusilukuTB
+            // 
+            this.UusilukuTB.Location = new System.Drawing.Point(259, 13);
+            this.UusilukuTB.Name = "UusilukuTB";
+            this.UusilukuTB.Size = new System.Drawing.Size(132, 35);
+            this.UusilukuTB.TabIndex = 1;
+            this.UusilukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(12, 62);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(83, 30);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Lukujenjarjestys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 101);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.UusilukuTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Lukujenjarjestys";
+            this.Text = "Lukujen järjestys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UusilukuTB;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
---
+++ 5. Harjoitus Lukujenjärjestys/5. Harjoitus Lukujenjärjestys/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5._Harjoitus_Lukujenjärjestys
+{
+    public partial class Lukujenjarjestys : Form
+    {
+        List<int> jono = new List<int>();
+        public Lukujenjarjestys()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (UusilukuTB.Text == "-999")
+                {
+                    VastausLB.Text = "";
+                    int[] taulukko = jono.ToArray();
+                    Array.Sort(taulukko);
+                    foreach (var jasen in taulukko)
+                    {
+                        VastausLB.Text += jasen + " ";
+                    }
+                    VastausLB.Visible = true;
+                }
+                else
+                {
+                    jono.Add(Int32.Parse(UusilukuTB.Text));
+                    UusilukuTB.Text = "";
+                }
+            }
+        }
+    }
+}
